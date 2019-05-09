4f43599604db140056ea2154777b55fe6aedbc4a
Daniel H <42782636+hirahokad@users.noreply.github.com>
2019-05-08 19:55:40 -0700

    Update clustersetup.sh

--- Daniel/PodLamp/clustersetup.sh
+++ Daniel/PodLamp/clustersetup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-KOPS_CLUSTER_NAME=potato2.k8s.local
-KOPS_STATE_STORE=s3://potato2-kop-state-store
+KOPS_CLUSTER_NAME=" "
+KOPS_STATE_STORE=" "
 
 kops create cluster --node-count=2 --node-size=t2.micro --zones=us-east-1a
 kops update cluster --name ${KOPS_CLUSTER_NAME} --yes
